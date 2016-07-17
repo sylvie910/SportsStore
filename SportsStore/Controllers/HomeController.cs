70eb52501e9eb40096acfe2a7abceea9a839bdb8
Xiaocao Hu <sylvie910@gmail.com>
2016-07-16 23:23:01 -0400

    display a list of products

---
+++ SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Abstract;
+
+namespace SportsStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IProductRepository repository;
+
+        public HomeController(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public ActionResult Index()
+        {
+            return View(repository.Products);
+        }
+    }
+}
