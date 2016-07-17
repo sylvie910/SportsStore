5a5b5a596b2be173c193ba4fab5a7030b8ba1144
Xiaocao Hu <sylvie910@gmail.com>
2016-07-17 13:24:00 -0400

    add pagination

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -4,21 +4,33 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SportsStore.Abstract;
+using SportsStore.Models;
 
 namespace SportsStore.Controllers
 {
     public class HomeController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
 
         public HomeController(IProductRepository repository)
         {
             this.repository = repository;
         }
 
-        public ActionResult Index()
+        public ViewResult Index(int page = 1)
         {
-            return View(repository.Products);
+            ProductListViewModel model = new ProductListViewModel
+            {
+                Products = repository.Products.OrderBy(p => p.ProductId).Skip((page - 1) * PageSize).Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
+            return View(model);
         }
     }
 }
---
+++ SportsStore/Models/ProductListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace SportsStore.Models
+{
+    public class ProductListViewModel : IEnumerable
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        public IEnumerator GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
