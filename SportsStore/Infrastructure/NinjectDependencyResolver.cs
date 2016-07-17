c9f18d09553a574e0d45112aa6ad37fe50fc2286
Xiaocao Hu <sylvie910@gmail.com>
2016-07-16 22:32:18 -0400

    Set up DI container

---
+++ SportsStore/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Ninject;
+
+namespace SportsStore.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel _kernel;
+
+        public NinjectDependencyResolver(IKernel kernel)
+        {
+            _kernel = kernel;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _kernel.GetService(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            //to do
+        }
+
+    }
+}
